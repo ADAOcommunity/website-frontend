f5a1981aac7976b1e1fbb24c7f58d9df84c936a3
zmontgo <31627113+zmontgo@users.noreply.github.com>
2022-05-19 14:58:34 -0500

    Stylistic bug fixes, section two

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,25 @@ export default function Home() {
     <Helmet>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link href="https://fonts.googleapis.com/css2?family=Cantarell:wght@800&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Cantarell:wght@400;700&display=swap" rel="stylesheet" />
     </Helmet>
 
     <Topnav></Topnav>
     <Hero></Hero>
+    <div class="section">
+      <div class="section__horizontal">
+        <div class="section__text">
+          <img src={'/10.png'} alt="" />
+        </div>
+        <div class="section__text">
+          <h5 class="bluetext">About ADAO</h5>
+          <h3>Driven By Decentralization</h3>
+          <p>At ADAO, we never stop thinking about decentralization. Our tools are designed to empower the community and promote decentralization throughout the ecosystem. We know that decentralization is a process, but we believe in the power of what we can accomplish if we work together.</p>
+
+          <p>With our combined efforts, organizations will be able to govern themselves in new ways and incentivize members for contributions, while staying connected globally, any time, any day. This allows for open, fair ways to participate and be heard that suit your group's needs, whenever and wherever. The decentralized communities you form using the tools that we build will ultimately be the greatest measure of our success.</p>
+        </div>
+      </div>
+    </div>
   </div>
 }
 
