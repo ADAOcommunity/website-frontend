1e9ad7459581bcea38ab39a77c56095a2a9d86c3
zmontgo <31627113+zmontgo@users.noreply.github.com>
2022-05-24 13:52:12 -0500

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,15 +4,35 @@ import Footer from "./imports/footer"
 import Head from "./imports/head"
 import Topnav from "./imports/topnav"
 
+import plug from "../components/img/assets/11.png"
+
 import "../components/style/style.scss"
 
 export default function Home() {
   return (
-    <div id="index">
+    <div>
       <Head></Head>
 
       <Topnav></Topnav>
 
+      <div className="section">
+        <div className="section__horizontal">
+          <div className="section__text fof">
+            <img src={plug} alt="" />
+          </div>
+          <div className="section__text">
+            <h1>404</h1>
+            <h3>Sorry, that page wasn't found!</h3>
+            
+            <a
+              className="btn vr"
+              href="/"
+            >
+              Back home
+            </a>
+          </div>
+        </div>
+      </div>
       
       <Footer></Footer>
     </div>
